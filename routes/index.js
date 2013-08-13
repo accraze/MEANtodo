77cf26f0d81bd69888e5e8a8e5482853c0918192
accraze <accraze@gmail.com>
2013-08-12 20:30:59 -0700

    wrote scenario test for index view

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,15 @@ exports.index = function(req, res){
     title: 'Express',
     todos : [
       { description : "Buy eggs",
-        due : new Date(new Date().getTime()  24 * 60 * 60 * 1000), // 1 day from now
+        due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 1 day from now
         done : false
       },
       { description : "Write next blog post",
-        due : new Date(new Date().getTime()  7 * 24 * 60 * 60 * 1000), // 1 week from now
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
         done : false
       },
       { description : "Build todo list app",
-        due : new Date(new Date().getTime()  7 * 24 * 60 * 60 * 1000), // 1 week from now
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
         done : true
       },
     ]
