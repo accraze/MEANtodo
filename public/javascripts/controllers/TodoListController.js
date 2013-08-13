c65ec27565e6c3ebcf4dae1d7dd0ead8a477d336
accraze <accraze@gmail.com>
2013-08-13 00:11:23 -0700

    apply Todo model

--- public/javascripts/controllers/TodoListController.js
+++ public/javascripts/controllers/TodoListController.js
@@ -1,10 +1,9 @@
 function TodoListController($scope, $http) {
   $scope.todos = [];
-
-  $scope.newTodo={
-  	done : false,
-  	due : new Date (new Date().getTime() + 24 * 60 * 60 * 1000),
-  	description : ''
+  $scope.newTodo = {
+    done : false,
+    due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+    description : ''
   };
 
   $scope.doneFilter = { done : true };
@@ -14,10 +13,14 @@ function TodoListController($scope, $http) {
     $scope.todos = todos;
   };
 
-  $scope.addNewTodo = function(){
-  	$http.post('/todo.json', $scope.newTodo).success(function(data){
-  		$scope.todos = data.todos;
-  		$scope.newTodo.description = '';
-  	});
+  $scope.addNewTodo = function() {
+    $http.post('/todo.json', $scope.newTodo).success(function(data) {
+      if (data.todo) {
+        $scope.todos.push(data.todo);
+        $scope.newTodo.description = '';
+      } else {
+        alert(JSON.stringify(data));
+      }
+    });
   };
 }
